91b9195e04fc7533e1b5993aae37c6cb32aed44c
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-01-18 15:26:57 +0000

    feat: Add a name to the `from` section of the email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: email_address_with_name("no-reply@hackathon.com", "Hackathon Team")
   layout "mailer"
 end
