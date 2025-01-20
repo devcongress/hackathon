47f7667cdf5d50636e0a3eedf449597795cbe0d5
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-01-20 11:33:59 +0000

    feat: Remove non-functional class action

--- packages/hacker_dashboard_portal/app/definitions/hacker_dashboard_portal/team_definition.rb
+++ packages/hacker_dashboard_portal/app/definitions/hacker_dashboard_portal/team_definition.rb
@@ -1,8 +1,3 @@
 class HackerDashboardPortal::TeamDefinition < ::Hackathon::TeamDefinition
   include HackerDashboardPortal::ResourceDefinition
-
-  class ShowPage < ShowPage
-    def render_breadcrumbs
-    end
-  end
 end
