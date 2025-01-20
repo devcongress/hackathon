261a6c265f82ca9e610a9376175d5136524fd677
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-01-20 11:30:49 +0000

    feat: Add seeds to the development for easy data generation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,46 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+return unless Rails.env.local?
+
+@password = BCrypt::Password.create("password")
+
+@admin = Admin.create(email: "admin@email.com", password_hash: @password,
+                      status: :verified)
+
+@data = {
+  thrusters: {
+    members: [
+      {
+        email: "joe@email.com", role: :product_manager, owner: true,
+        name: "John Doe"
+      },
+      { email: "esi@email.com", role: :designer, name: "Esi Puma" },
+      { email: "bob@email.com", role: :developer, name: "Bob Sickle" }
+    ]
+  },
+  curators: {
+    members: [
+      {
+        email: "sally@email.com", role: :developer, owner: true,
+        name: "Sally Banks"
+      },
+      { email: "kane@email.com", role: :designer, name: "Kane Rogers" },
+      { email: "lisa@email.com", role: :developer, name: "Lisa Brooks" }
+    ]
+  }
+}
+
+@data.each do |team, data|
+  @team_name = team.to_s.capitalize
+
+  data[:members].each do |member|
+    @hacker = Hacker.create!(email: member[:email], password_hash: @password,
+                             status: :verified)
+    if member[:owner]
+      @team = Hackathon::Team.create!(name: @team_name, hacker_id: @hacker.id)
+    end
+
+    @profile = Profile.create!(
+      name: member[:name], team: @team,
+      hacker: @hacker, role: member[:role],
+    )
+  end
+end
