08ae485f1d7bc138685475ba3cc31b655f86a82d
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-01-20 20:30:59 +0000

    feat: Update invitation link to newer version with token

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -3,15 +3,9 @@ class InvitationMailer < ApplicationMailer
     @invited_hacker = params[:hacker]
     @team = @invited_hacker.team
     @owner = @team.owner
-    @url = hacker.create_account_url(token: @invited_hacker.token)
+    @url = confirm_invitation_url(token: @invited_hacker.token)
 
     mail to: @invited_hacker.email,
          subject: "Join '#{@team.name}' for the Hackathon!"
   end
-
-  private
-
-    def hacker
-      RodauthApp.rodauth(:hacker).allocate
-    end
 end
