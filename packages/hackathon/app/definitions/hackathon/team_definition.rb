49ad147ddf88a0597b4bbdc9cacc6f08875a81c2
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-01-20 11:32:48 +0000

    feat: Show breadcrumbs only for Admins when viewing team info

--- packages/hackathon/app/definitions/hackathon/team_definition.rb
+++ packages/hackathon/app/definitions/hackathon/team_definition.rb
@@ -1,2 +1,9 @@
 class Hackathon::TeamDefinition < Hackathon::ResourceDefinition
+  class ShowPage < ShowPage
+    def render_breadcrumbs
+      if current_user.class == Admin
+        super
+      end
+    end
+  end
 end
