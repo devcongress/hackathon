78210668505c48e5c8beabed60bc80fb3942110a
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-01-20 12:01:40 +0000

    feat: Add the initial implementation to register invited hackers
    This update fetches the `invite_token` that is cached when the hacker
    visits the accounts creation route and registers successfully. Once they
    are registered, the token (which was sent) by their team leader is used
    for further processing.
    
    This will be updated to send confirmation emails to the new hacker and
    the team lead of this event.

--- packages/hacker_dashboard_portal/app/controllers/hacker_dashboard_portal/profiles_controller.rb
+++ packages/hacker_dashboard_portal/app/controllers/hacker_dashboard_portal/profiles_controller.rb
@@ -5,23 +5,67 @@ class HackerDashboardPortal::ProfilesController < ::ProfilesController
 
   def create
     @profile = ::Profile.new(profile_params.except(:team))
-    @team = GlobalID::Locator.locate_signed(profile_params[:team])
-    @profile.team = @team
+    invite_token = fetch_invite_token
 
-    authorize_current!(@profile)
+    authorize_current! @profile
+
+    # When the token key was fetched but the content is empty, there's a problem
+    if invite_token && invite_token.empty?
+      return redirect_to_new_profile_with_alert("The token could not be found!")
+    end
+
+    team = find_team(invite_token)
+    @profile.team = team
+
+    Rails.logger.debug "Profile info after authorize: #{@profile.inspect}"
 
     if @profile.save
-      redirect_to(@profile, notice: "Profile was successfully created.")
+      handle_successful_save(invite_token)
     else
-      flash.now[:alert] = "Profile could not be created."
-      render(:new)
+      redirect_to_new_profile_with_alert(
+        "There was an error while creating the profile.\n" \
+        "#{@profile.errors.full_messages.join("\n")}"
+      )
     end
   end
 
   private
 
     def profile_params
-      params.expect(profile: [ :name, :role, :telephone_number,
-:team ]).merge({ hacker: current_user })
+      params
+        .expect(profile: [ :name, :role, :telephone_number, :team ])
+        .merge({ hacker: current_user })
+    end
+
+    def fetch_invite_token
+      Rails.cache.read("#{current_user.email}_invite_token")
+    end
+
+    def find_team(invite_token)
+      @invited_hacker = Hackathon::Invitation.includes(:team)
+                                             .find_by(token: invite_token)
+
+      if @invited_hacker
+        @invited_hacker.team
+      else
+        GlobalID::Locator.locate_signed(profile_params[:team])
+      end
+    end
+
+    def handle_successful_save(invite_token)
+      if invited_hacker?
+        @invited_hacker.update!(accepted: true)
+        Rails.cache.delete(@invited_hacker.token)
+      end
+
+      redirect_to(@profile, notice: "Profile was successfully created.")
+    end
+
+    def redirect_to_new_profile_with_alert(message)
+      redirect_to resource_url_for(::Profile, action: :new), alert: message
+    end
+
+    def invited_hacker?
+      @invited_hacker
     end
 end
