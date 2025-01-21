6e7582f029f12601c6f477e51438679ee68194e6
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-01-21 23:18:00 +0000

    feat: Skip checking the hacker's team during a fresh account creation

--- packages/hacker_dashboard_portal/app/controllers/hacker_dashboard_portal/hackathon/teams_controller.rb
+++ packages/hacker_dashboard_portal/app/controllers/hacker_dashboard_portal/hackathon/teams_controller.rb
@@ -1,7 +1,7 @@
 class HackerDashboardPortal::Hackathon::TeamsController < ::Hackathon::TeamsController
   include HackerDashboardPortal::Concerns::Controller
 
-  skip_before_action :ensure_joined_team, only: [:new, :create]
+  skip_before_action :ensure_joined_team, only: [ :new, :create ]
 
   def present_scoped_entity? = true
 end
--- packages/hacker_dashboard_portal/app/controllers/hacker_dashboard_portal/profiles_controller.rb
+++ packages/hacker_dashboard_portal/app/controllers/hacker_dashboard_portal/profiles_controller.rb
@@ -1,5 +1,6 @@
 class HackerDashboardPortal::ProfilesController < ::ProfilesController
   include HackerDashboardPortal::Concerns::Controller
 
-  skip_before_action :ensure_profile_complete, only: [:new, :create]
+  skip_before_action :ensure_profile_complete, only: [ :new, :create ]
+  skip_before_action :ensure_joined_team, only: [ :new, :create ]
 end
