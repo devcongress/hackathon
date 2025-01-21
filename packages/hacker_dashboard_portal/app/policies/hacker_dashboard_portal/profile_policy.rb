64f6f20517b4f20700ddd5b8de09425239a4a5e8
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-01-21 23:30:06 +0000

    feat: Allow new profile creation only when one does exist already

--- packages/hacker_dashboard_portal/app/policies/hacker_dashboard_portal/profile_policy.rb
+++ packages/hacker_dashboard_portal/app/policies/hacker_dashboard_portal/profile_policy.rb
@@ -1,5 +1,9 @@
 module HackerDashboardPortal
   class ProfilePolicy < ::ProfilePolicy
     include HackerDashboardPortal::ResourcePolicy
+
+    def new?
+      user.profile.nil?
+    end
   end
 end
