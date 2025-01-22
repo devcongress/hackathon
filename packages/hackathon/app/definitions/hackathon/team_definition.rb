6fca3fb58ce4ea98733ee086551079bd8bba0f42
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-01-22 02:04:05 +0000

    chore: Remove unneeded code snippets

--- packages/hackathon/app/definitions/hackathon/team_definition.rb
+++ packages/hackathon/app/definitions/hackathon/team_definition.rb
@@ -1,9 +1,2 @@
 class Hackathon::TeamDefinition < Hackathon::ResourceDefinition
-  class ShowPage < ShowPage
-    def render_breadcrumbs
-      if current_user.class == Admin
-        super
-      end
-    end
-  end
 end
--- packages/hackathon/app/models/hackathon/team.rb
+++ packages/hackathon/app/models/hackathon/team.rb
@@ -24,20 +24,17 @@ class Hackathon::Team < Hackathon::ResourceRecord
 
   has_many :team_memberships, dependent: :destroy
   has_many :hackers, through: :team_memberships
-  has_many :invitations, class_name: "Hackathon::Invitation", dependent: :destroy
+  has_many :invitations, class_name: "Hackathon::Invitation",
+                         dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
 
   attribute :role
-  validates :role, presence: true, inclusion: {in: Hackathon::TeamMembership.roles.keys}, on: :create
+  validates :role, presence: true,
+           inclusion: { in: Hackathon::TeamMembership.roles.keys },
+           on: :create
 
   after_create do
     team_memberships.create!(hacker: hacker, role:)
   end
-
-  # scope :associated_with_hacker,
-  #       ->(hacker) {
-  #         includes(:hackers)
-  #           .where(hackers: { team_id: hacker.team&.id })
-  #       }
 end
