c58cd0400958cf8beb238d406bac5a864005d61f
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-01-22 03:18:50 +0000

    feat: Update scope to include only hackers in the current team
    This update also disables the creating team memberships from the
    dashboards. The reason for this is because team membership is built only
    through member invitations. Once they join, they become part of the
    team.

--- packages/hacker_dashboard_portal/app/policies/hacker_dashboard_portal/hackathon/team_membership_policy.rb
+++ packages/hacker_dashboard_portal/app/policies/hacker_dashboard_portal/hackathon/team_membership_policy.rb
@@ -1,11 +1,28 @@
 class HackerDashboardPortal::Hackathon::TeamMembershipPolicy < ::Hackathon::TeamMembershipPolicy
   # Core attributes
 
+  def create?
+    false
+  end
+
+  def update?
+    user == record.hacker
+  end
+
+  def destroy?
+    false
+  end
+
   def permitted_attributes_for_create
-    [:hacker, :role]
+    [ :role ]
   end
 
   def permitted_attributes_for_read
-    [:hacker, :role]
+    [ :hacker, :role ]
+  end
+
+  relation_scope do |relation|
+    next relation unless entity_scope
+    relation.where(team: user.team)
   end
 end
