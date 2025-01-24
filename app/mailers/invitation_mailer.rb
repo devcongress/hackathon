32a402a6b38322c096c42016e3ca927962ffd952
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-01-24 14:41:44 +0000

    feat: Update invitation mailer to change invitation status based on the outcome

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -5,19 +5,29 @@ class InvitationMailer < ApplicationMailer
     @url = confirm_invitation_url(token: @invited_hacker.token)
 
     mail to: @invited_hacker.email,
-      subject: "Join '#{@team.name}' for the Hackathon!"
+         subject: "Join '#{@team.name}' for the Hackathon!"
+  rescue StandardError => e
+    Rails.logger.error("Failed to send invitation email: #{e.message}")
+    @invitation.failed!
+  else
+    @invitation.sent!
   end
 
   def confirm_invite
     mail to: email_address_with_name(@owner.email, @invited_hacker.name),
-      subject: "Registration Complete!"
+         subject: "Registration Complete!"
+
+    @invitation.accepted!
   end
 
   private
 
-  def set_details
-    @invited_hacker = params[:hacker]
-    @team = @invited_hacker.team
-    @owner = @team.hacker
-  end
+    def set_details
+      @invited_hacker = params[:hacker]
+      @team = @invited_hacker.team
+      @owner = @team.hacker
+
+      # An alias for invited hacker
+      @invitation = @invited_hacker
+    end
 end
--- packages/hacker_dashboard_portal/app/controllers/hacker_dashboard_portal/hackathon/team_memberships_controller.rb
+++ packages/hacker_dashboard_portal/app/controllers/hacker_dashboard_portal/hackathon/team_memberships_controller.rb
@@ -4,14 +4,9 @@ class HackerDashboardPortal::Hackathon::TeamMembershipsController < ::Hackathon:
   authorize :invitation, through: :current_invitation
 
   skip_before_action :ensure_joined_team, only: [ :new, :create ]
+  after_action :handle_success, only: [ :create ]
 
-  def current_invitation
-    return unless cookies.encrypted[:invite_token]
-
-    @invitation ||= ::Hackathon::Invitation.find_by(
-      token: cookies.encrypted[:invite_token],
-    )
-  end
+  def present_scoped_entity? = true
 
   def set_page_title(title)
     if title == "Create #{resource_class.model_name.human.titleize}"
@@ -20,4 +15,29 @@ class HackerDashboardPortal::Hackathon::TeamMembershipsController < ::Hackathon:
       super
     end
   end
+
+  private
+
+    def handle_success
+      delete_invite_cookie
+      check_team_validity
+    end
+
+    def current_invitation
+      return unless cookies.encrypted[:invite_token]
+
+      @invitation ||= ::Hackathon::Invitation.find_by(
+        token: cookies.encrypted[:invite_token],
+      )
+    end
+
+    def delete_invite_cookie
+      cookies.delete(:invite_token)
+    end
+
+    def check_team_validity
+      if @invitation.team.validated?
+        TeamMailer.with(team: current_invitation.team).validated.deliver_later
+      end
+    end
 end
