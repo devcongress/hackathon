98d1dbe60f418191570a0a38c0fdf67c72672d96
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-01-25 12:55:04 +0000

    feat: Make `params` available in the AdminPolicy for easy queries

--- packages/admin_dashboard_portal/app/controllers/admin_dashboard_portal/hackers_controller.rb
+++ packages/admin_dashboard_portal/app/controllers/admin_dashboard_portal/hackers_controller.rb
@@ -1,3 +1,11 @@
 class AdminDashboardPortal::HackersController < ::HackersController
   include AdminDashboardPortal::Concerns::Controller
+
+  authorize :params, through: :request_params
+
+  private
+
+    def request_params
+      params
+    end
 end
