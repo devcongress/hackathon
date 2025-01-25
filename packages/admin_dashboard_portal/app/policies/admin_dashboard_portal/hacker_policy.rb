36d7bd92c2f2124cefea4228196ed7a7af546251
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-01-25 12:57:20 +0000

    feat: Add policy for hacker relation scope in the admin dashboard

---
+++ packages/admin_dashboard_portal/app/policies/admin_dashboard_portal/hacker_policy.rb
@@ -0,0 +1,20 @@
+module AdminDashboardPortal
+  class HackerPolicy < ::HackerPolicy
+    authorize :params, optional: true
+    include AdminDashboardPortal::ResourcePolicy
+
+    def permitted_attributes_for_read
+      [ :email ]
+    end
+
+    # Scope hackers based on the team they belong to
+    relation_scope do |relation|
+      if params && params[:hackathon_team_id]
+        team_id = params[:hackathon_team_id]
+        relation.includes(:team).where(team: { id: team_id })
+      else
+        relation
+      end
+    end
+  end
+end
