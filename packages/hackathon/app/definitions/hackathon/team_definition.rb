a6ec0cc6481299a69a19cdf5e9875db631b007ad
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-01-30 13:14:15 +0000

    feat: Add description on the status field to display qualification criteria

--- packages/hackathon/app/definitions/hackathon/team_definition.rb
+++ packages/hackathon/app/definitions/hackathon/team_definition.rb
@@ -1,6 +1,8 @@
 class Hackathon::TeamDefinition < Hackathon::ResourceDefinition
   display :status, formatter: ->(value) { value.titleize }
 
+  field :status, description: "3 members are required to qualify"
+
   scope :qualified do |scope|
     scope.qualified
   end
