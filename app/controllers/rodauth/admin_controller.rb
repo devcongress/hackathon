60589635e2ae544ef61adc527adcc4d739254762
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-02-01 16:57:17 +0000

    chore: Remove include for ApplicationHelper

--- app/controllers/rodauth/admin_controller.rb
+++ app/controllers/rodauth/admin_controller.rb
@@ -1,6 +1,5 @@
 module Rodauth
   class AdminController < RodauthController
-    inlcude ApplicationHelper
     # used by Rodauth for rendering views, CSRF protection, and running any
     # registered action callbacks and rescue_from handlers
 
