bf6a40cb0b4c49e41a18a879c32756a133f139c3
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-02-02 14:48:58 +0000

    feat: Update the page title and description for hackers on admin dashboard

--- packages/admin_dashboard_portal/app/definitions/admin_dashboard_portal/hacker_definition.rb
+++ packages/admin_dashboard_portal/app/definitions/admin_dashboard_portal/hacker_definition.rb
@@ -1,10 +1,15 @@
 class AdminDashboardPortal::HackerDefinition < ::HackerDefinition
+  index_page_title "Hackers & Check In"
+
+  index_page_description "Validated team  hackers can be checked in upon " \
+    "arrival. Use the 'Check In' action when needed."
+
   search do |scope, query|
     scope.joins(:profile)
-         .where(
-           "LOWER(profiles.name) LIKE LOWER(:query)
+      .where(
+        "LOWER(profiles.name) LIKE LOWER(:query)
               OR LOWER(hackers.email) LIKE LOWER(:query)",
-           query: "%#{query}%",
-         )
+        query: "%#{query}%"
+      )
   end
 end
