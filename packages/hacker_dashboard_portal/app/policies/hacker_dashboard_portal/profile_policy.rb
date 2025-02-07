af2f33beb45a3eb6e31495d646f965c04d8cfc43
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-02-07 23:15:31 +0000

    feat: Ensure only profile owners can update their information

--- packages/hacker_dashboard_portal/app/policies/hacker_dashboard_portal/profile_policy.rb
+++ packages/hacker_dashboard_portal/app/policies/hacker_dashboard_portal/profile_policy.rb
@@ -5,6 +5,14 @@ module HackerDashboardPortal
     def new?
       user.profile.nil?
     end
+
+    def create?
+      true
+    end
+
+    def update?
+      user.profile == record
+    end
     def permitted_associations
       %i[emergency_contact] if user.profile == record
     end
