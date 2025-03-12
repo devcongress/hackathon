b6c57fe2770d1a61d1714155e132259307dfb6d2
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-03-12 12:35:32 +0000

    chore: Update the description for checking in a hacker

--- packages/admin_dashboard_portal/app/definitions/admin_dashboard_portal/hacker_definition.rb
+++ packages/admin_dashboard_portal/app/definitions/admin_dashboard_portal/hacker_definition.rb
@@ -2,7 +2,7 @@ class AdminDashboardPortal::HackerDefinition < ::HackerDefinition
   index_page_title "Hackers & Check In"
 
   index_page_description "Validated team  hackers can be checked in upon " \
-    "arrival. Use the 'Check In' action when needed."
+                         "arrival. Use the 'Check In' action only when needed."
 
   search do |scope, query|
     scope.joins(:profile)
