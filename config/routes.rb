4d4b53c04fd2cee7116b2bbdeda2aa680e9c7b24
Bubunyo Nyavor <kidd.bubu@gmail.com>
2025-09-29 18:54:17 +0200

    set default route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", :as => :rails_health_check
 
   # resources :homepage, only: [:index]
-  root "homepage#index"
+  root to: redirect('/hacker/login')
   get "/about", to: "homepage#about"
   get "/contact", to: "homepage#contact"
 
