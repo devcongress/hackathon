2d65440cf1323af31bef089b3d0445e579b6186a
Bubunyo Nyavor <kidd.bubu@gmail.com>
2025-09-29 11:51:59 +0200

    change logos

--- config/initializers/plutonium.rb
+++ config/initializers/plutonium.rb
@@ -5,7 +5,7 @@ Plutonium.configure do |config|
 
   config.assets.stylesheet = "application"
   config.assets.script = "application"
-  config.assets.logo = "Logo-Monochrome.svg"
+  config.assets.logo = "neural-network.png"
   config.assets.favicon = "favicon.ico"
   # Configure plutonium above.
 end
