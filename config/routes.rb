3494c3acb088ef7ed9438dfe99ed67d6781d3de6
Bubunyo Nyavor <kidd.bubu@gmail.com>
2025-10-03 15:26:06 +0200

    Feature/landing page (#12)
    * setup basic fly deploy
    
    * trigger deploy and push to stage
    
    * align naming
    
    * change logos
    
    * add new logo
    
    * enable landing page
    
    * fix test

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", :as => :rails_health_check
 
-  # resources :homepage, only: [:index]
-  root to: redirect("/hacker/login")
+  resources :homepage, only: [:index]
+  # root to: redirect("/hacker/login")
   get "/about", to: "homepage#about"
   get "/contact", to: "homepage#contact"
 
