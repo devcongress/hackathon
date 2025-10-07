044a15018ac1542c6dc040745ce34bb2ed58e431
Bubunyo Nyavor <kidd.bubu@gmail.com>
2025-10-07 10:12:20 +0200

    home page branding

--- config/initializers/plutonium.rb
+++ config/initializers/plutonium.rb
@@ -5,7 +5,7 @@ Plutonium.configure do |config|
 
   config.assets.stylesheet = "application"
   config.assets.script = "application"
-  config.assets.logo = "neural-network.png"
+  config.assets.logo = "100_x_500_logo.png"
   config.assets.favicon = "favicon.ico"
   # Configure plutonium above.
 end
