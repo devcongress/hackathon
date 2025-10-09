fb2c6442b6f58ed158bca54f8d02e5dd046156a6
Bubunyo Nyavor <kidd.bubu@gmail.com>
2025-10-09 11:41:29 +0200

    make modifications to copy

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,7 +2,23 @@ class ApplicationMailer < ActionMailer::Base
   default from: "UNICEF StartUp Lab Hack <hack25@devcongress.io>"
   layout "mailer"
 
+  def self.hack_name
+    "The UNICEF StartUp Lab Hack"
+  end
+
   def self.event_date
     "Thursday, November 12, 2025"
+  end
+
+  def self.event_time
+    "2pm GMT"
+  end
+
+  def self.location
+    "[TBD]"
+  end
+
+  def self.location_map
+    "[TBD]"
   end
 end
