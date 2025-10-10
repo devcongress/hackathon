7e36e119f5e67c3f1147202df96e0455dfdca248
TheDumbTechGuy <sfroelich01@gmail.com>
2025-10-10 21:45:33 +0000

    add dark mode for new dashboard views

--- packages/hacker_dashboard_portal/app/components/hacker_dashboard_portal/resource_layout.rb
+++ packages/hacker_dashboard_portal/app/components/hacker_dashboard_portal/resource_layout.rb
@@ -1,7 +1,7 @@
 module HackerDashboardPortal
   class ResourceLayout < ::Plutonium::UI::Layout::ResourceLayout
     def main_attributes = mix(super, {
-      class!: "pt-20 p-6 min-h-screen mx-auto md:w-2/3"
+      class!: "pt-20 p-6 min-h-screen mx-auto md:w-2/3 dark:bg-gray-900"
     })
   end
 end
