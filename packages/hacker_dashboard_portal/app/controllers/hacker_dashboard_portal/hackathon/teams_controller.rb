4520ce5cfe628774f71c29fdb73bf8ea3e75aade
TheDumbTechGuy <sfroelich01@gmail.com>
2025-10-15 10:15:03 +0000

    do not expose teams index page

--- packages/hacker_dashboard_portal/app/controllers/hacker_dashboard_portal/hackathon/teams_controller.rb
+++ packages/hacker_dashboard_portal/app/controllers/hacker_dashboard_portal/hackathon/teams_controller.rb
@@ -5,6 +5,17 @@ class HackerDashboardPortal::Hackathon::TeamsController < ::Hackathon::TeamsCont
 
   def present_scoped_entity? = true
 
+  def index
+    authorize_current! resource_class
+    skip_verify_current_authorized_scope!
+
+    if current_user.team
+      redirect_to resource_url_for(current_user.team)
+    else
+      redirect_to root_path
+    end
+  end
+
   def new
     authorize_current! resource_class
 
