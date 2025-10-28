63caf3ac7d950f5d1a4a41ae4294380f1c4265c0
TheDumbTechGuy <sfroelich01@gmail.com>
2025-10-28 00:40:57 +0000

    use markdown for project submission

--- packages/hackathon/app/definitions/hackathon/project_submission_definition.rb
+++ packages/hackathon/app/definitions/hackathon/project_submission_definition.rb
@@ -1,10 +1,10 @@
 class Hackathon::ProjectSubmissionDefinition < Hackathon::ResourceDefinition
-  field :about_project, as: :text,
+  field :about_project, as: :markdown,
     label: "About the Project",
     hint: "Describe your project: What problem does it solve? What features does it have? What makes it special?"
   display :about_project, wrapper: {class: "col-span-full"}
 
-  field :about_team, as: :text,
+  field :about_team, as: :markdown,
     label: "About the Team",
     hint: "Tell us about your team: What brought you together? What's your story? What did you learn?"
   display :about_team, wrapper: {class: "col-span-full"}
