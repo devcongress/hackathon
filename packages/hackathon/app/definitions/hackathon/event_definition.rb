f289fdcacea20a0ca9930aa02e2c3e63351b33d8
TheDumbTechGuy <sfroelich01@gmail.com>
2025-10-28 02:36:44 +0000

    display events on hacker dashboard

--- packages/hackathon/app/definitions/hackathon/event_definition.rb
+++ packages/hackathon/app/definitions/hackathon/event_definition.rb
@@ -1,4 +1,6 @@
 class Hackathon::EventDefinition < Hackathon::ResourceDefinition
+  field :duration, hint: "Duration in minutes", description: "Duration in minutes"
+
   display :meeting_link, as: :url
   display :video_url, as: :url
 
