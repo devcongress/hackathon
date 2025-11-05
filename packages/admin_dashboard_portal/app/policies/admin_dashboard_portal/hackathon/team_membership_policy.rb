0f8b3d194793e206bf1b6041246a8e1134ca55b4
TheDumbTechGuy <sfroelich01@gmail.com>
2025-11-05 15:59:33 +0000

    allow team member deletions

--- packages/admin_dashboard_portal/app/policies/admin_dashboard_portal/hackathon/team_membership_policy.rb
+++ packages/admin_dashboard_portal/app/policies/admin_dashboard_portal/hackathon/team_membership_policy.rb
@@ -4,4 +4,9 @@ class AdminDashboardPortal::Hackathon::TeamMembershipPolicy < ::Hackathon::TeamM
   def create?
     false
   end
+
+  def destroy?
+    # Allow destroying team memberships, but not the team owner's membership
+    record.team.hacker_id != record.hacker_id
+  end
 end
